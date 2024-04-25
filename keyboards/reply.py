db70a33c20ac21e225df323fd49235147826be40
sayrekas <chaotichoice@gmail.com>
2024-04-25 15:58:20 +0300

    Bugfix Optimizations Added new button - gender_btn

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -3,13 +3,13 @@ from settings import muscle_group_list, db, user, con
 from aiogram.utils.keyboard import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardBuilder
 
 
-def create_reply_kbs(button_name_list: list[str]):
+def create_reply_kbs(button_name_list: list[str], ):
     keyboards = []
 
     for item in button_name_list:
-        keyboards.append([KeyboardButton(text=item, callback_data=item)])
+        keyboards.append([KeyboardButton(text=item, callback_data=item, )])
 
-    result = ReplyKeyboardMarkup(keyboard=keyboards, resize_keyboard=True)
+    result = ReplyKeyboardMarkup(keyboard=keyboards, resize_keyboard=True, )
 
     return result
 
@@ -18,15 +18,16 @@ class ReplyKb:
     start_menu = ReplyKeyboardMarkup(keyboard=[
         [KeyboardButton(text='Вказати параметри тіла', )],
     ], resize_keyboard=True,
-        input_field_placeholder='тестовий текст'
     )
 
     main_menu = ReplyKeyboardMarkup(keyboard=[
         [KeyboardButton(text='Почати тренування', )],
-        [KeyboardButton(text='Додати нове тренування')],
+        [KeyboardButton(text='Додати нову вправу')],
         [KeyboardButton(text='Налаштування')],
     ], resize_keyboard=True,
-        input_field_placeholder='тестовий текст'
     )
-
-    muscle_group = create_reply_kbs(db.get_muscle_groups())
+    gender_btn = ReplyKeyboardMarkup(keyboard=[
+        [KeyboardButton(text='👨‍💼Чоловік', )],
+        [KeyboardButton(text='👩‍💼Жінка')],
+    ], resize_keyboard=True,
+    )
