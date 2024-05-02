61930cf7f7e70000203d630ac94e15e606566f57
sayrekas <chaotichoice@gmail.com>
2024-05-02 11:40:57 +0300

    Added new keyboards: settings_menu, another_training. Added lists for buttons: repeat_step_1_30, weight_step_1_30. The create_reply_kbs function can now group or ungroup buttons.

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -1,16 +1,23 @@
-from aiogram.types import ReplyKeyboardRemove
-from settings import muscle_group_list, db, user, con
-from aiogram.utils.keyboard import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardBuilder
+from aiogram.utils.keyboard import ReplyKeyboardMarkup, KeyboardButton
+
+repeat_step_1_30 = range(1, 51)
+
+weight_step_1_30 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30,
+                    32, 34, 36, 38, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
 
 
-def create_reply_kbs(button_name_list: list[str], ):
+def create_reply_kbs(button_name_list: list[str], group: int = None):
     keyboards = []
 
-    for item in button_name_list:
-        keyboards.append([KeyboardButton(text=item, callback_data=item, )])
+    if group:
+        sep_keyboards = [button_name_list[i:i + group] for i in range(0, len(button_name_list), group)]
+        for item in sep_keyboards:
+            keyboards.append([KeyboardButton(text=str(f)) for f in item])
+    else:
+        for item in button_name_list:
+            keyboards.append([KeyboardButton(text=str(item))])
 
-    result = ReplyKeyboardMarkup(keyboard=keyboards, resize_keyboard=True, )
-
+    result = ReplyKeyboardMarkup(keyboard=keyboards, resize_keyboard=True)
     return result
 
 
@@ -26,8 +33,22 @@ class ReplyKb:
         [KeyboardButton(text='Налаштування')],
     ], resize_keyboard=True,
     )
+
+    settings_menu = ReplyKeyboardMarkup(keyboard=[
+        [KeyboardButton(text='Вказати параметри тіла', )],
+        [KeyboardButton(text='Видалити вправу')],
+        [KeyboardButton(text='Головне меню')],
+    ], resize_keyboard=True,
+    )
+
     gender_btn = ReplyKeyboardMarkup(keyboard=[
         [KeyboardButton(text='👨‍💼Чоловік', )],
         [KeyboardButton(text='👩‍💼Жінка')],
+    ], resize_keyboard=True,
+    )
+
+    another_training = ReplyKeyboardMarkup(keyboard=[
+        [KeyboardButton(text='Додати ще один підхід', )],
+        [KeyboardButton(text='Головне меню', )],
     ], resize_keyboard=True,
     )
