645736eb463057a4a9e4363dbb8ce17a4c288486
sayrekas <chaotichoice@gmail.com>
2024-05-06 21:14:39 +0300

    Обробка виключень та помилок під час роботи з ботом Виправлення помилок Оптимізація

--- FSM/FSM_new_exercise.py
+++ FSM/FSM_new_exercise.py
@@ -10,6 +10,7 @@ from keyboards.reply import ReplyKb, create_reply_kbs
 FSM_add_new_exercise = Router()
 
 
+# реакція на натискання кнопккі 'Додати нову вправу' ___________________________________________________________________
 @FSM_add_new_exercise.message(StateFilter(None), F.text == 'Додати нову вправу')
 async def func(message: Message, state: FSMContext):
     await state.set_state(NewExerciseState.muscle_group_name)
@@ -17,13 +18,20 @@ async def func(message: Message, state: FSMContext):
                          reply_markup=create_reply_kbs(gh.get_muscle_groups(), group=3, additional_btn=['🚫Відмінити']))
 
 
+# вибір групи мʼязів ___________________________________________________________________________________________________
 @FSM_add_new_exercise.message(NewExerciseState.muscle_group_name, F.text)
 async def func(message: Message, state: FSMContext):
-    await state.update_data(muscle_group_name=message.text)
-    await message.answer('Вкажіть назву вправи:', reply_markup=ReplyKb.cancel_btn)
-    await state.set_state(NewExerciseState.exercise_name)
+    if message.text in gh.get_muscle_groups():
+        await state.update_data(muscle_group_name=message.text)
+        await message.answer('Вкажіть назву вправи:', reply_markup=ReplyKb.cancel_btn)
+        await state.set_state(NewExerciseState.exercise_name)
+    else:
+        await message.answer('Виберіть праильний варіант',
+                             reply_markup=create_reply_kbs(gh.get_muscle_groups(), group=3,
+                                                           additional_btn=['🚫Відмінити']))
 
 
+# вибір назви вправи ___________________________________________________________________________________________________
 @FSM_add_new_exercise.message(NewExerciseState.exercise_name, F.text)
 async def func(message: Message, state: FSMContext):
     await state.update_data(exercise_name=message.text)
