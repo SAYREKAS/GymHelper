1c70fca464ab97bf54bec3aec4ce931e9d6d2f07
sayrekas <chaotichoice@gmail.com>
2024-05-07 12:16:33 +0300

    Тепер функція "is_number" віддає True тільки якщо число не більше за аргумент "length" Виправлення помилок Оптимізація

--- FSM/common_functions.py
+++ FSM/common_functions.py
@@ -1,6 +1,21 @@
-def is_number(stroke: str) -> bool:
+from aiogram.fsm.context import FSMContext
+from aiogram.types import Message
+
+
+def is_number(stroke: str, lenght: int) -> bool:
     try:
-        float(stroke.replace(',', '.'))
-        return True
+        num = float(stroke.replace(',', '.'))
+        if lenght >= num > 0:
+            return True
     except ValueError:
         return False
+
+
+async def update_and_notify(message: Message, state: FSMContext, field: str, value: str, redact_message: int = None):
+    await state.update_data({field: value})
+    data = await state.get_data()
+    if not redact_message:
+        await message.answer(f"Група мʼязів - {data.get('muscle_group') if data.get('muscle_group') else '❔'}\n"
+                             f"Вправа - {data.get('exercise') if data.get('exercise') else '❔'}\n"
+                             f"Вага - {data.get('weight') if data.get('weight') else '❔'} кг\n"
+                             f"Повтори - {data.get('repeats') if data.get('repeats') else '❔'} раз\n")
